efb33fb04ae848a8fc0aa11e45ec103f31b3bf90
Kwadwo <kwadikap@gmail.com>
2022-05-29 16:16:07 -0400

    updated settings_py

--- internapi/settings.py
+++ internapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h!7!3ej1ms82s@=x8=$&@40tka7^p6r7^85-101z9o0@g0pt&b'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
